5659e7e2dabf8a988fdc9b7f2a4b4ba38d32daf5
gowdachandu5 <chandugowda5667@gmail.com>
2025-07-23 11:46:18 +0530

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,11 @@
+  428 B.E.T. Polytechnic College recognized by Government of Karnataka and approved by AICTE New Delhi was started in 1986 by Dr.G.Madegowda, our Founder Chairman who is a renowned visionary, philanthropist, and a pioneer in creating several landmark infrastructure educational Institutions in Mandya District.
+  Dr.G.Madegowda envisioned B.E.T.Polytechnic as an excellence imparting quality and affordable education. Today, B.E.T.Polytechnic stands tall as one of Karnataka’s finest names in Diploma Engineering Education and has produced more than 5,000 engineering professionals who occupy responsible positions across the country and globe.
+
+
+308 BET Polytechnic College stands out for its commitment to excellence in education. With a rich history dating back to 1986, it has consistently provided top-notch education in the field of engineering. The college boasts a dedicated faculty who are experts in their respective fields, ensuring that students receive the best possible guidance and mentorship. Additionally, the college's state-of-the-art infrastructure and facilities provide students with an enriching learning environment.
+
+324 BET Polytechnic College is renowned for providing top-notch education. Our curriculum is designed to meet industry standards and equip students with the skills and knowledge necessary for success.
+
+332Our college boasts a team of expert faculty who are dedicated to nurturing the talents of our students. They bring a wealth of experience and knowledge to the classroom, ensuring that students receive the best possible education.
+
+340At BET Polytechnic College, we understand the importance of providing students with access to state-of-the-art facilities. Our campus is equipped with modern laboratories, libraries, and other amenities to support their learning journey.
